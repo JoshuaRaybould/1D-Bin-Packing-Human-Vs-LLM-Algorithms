322b9f0e906db073758d23ec1cf5320b4d170290
JoshuaRaybould <joshua.raybould@outlook.com>
2025-10-28 21:53:54 +0000

    update main to print algorithm runtime

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import load_data
+import time
 from algorithms import randomised_best_fit
 
 done = False
@@ -7,11 +8,11 @@ while not done:
     selection = int(input("Type a number 1-4 "))
 
     if selection == 1:
-        instances = load_data.getRandomInstances(True)
+        instances = load_data.getRandomInstances()
     elif selection == 2:
-        instances = load_data.getFalkenauer(True)
+        instances = load_data.getFalkenauer()
     elif selection == 3:
-        instances = load_data.getHardInstances(True, False)
+        instances = load_data.getHardInstances(True, True)
     elif selection == 4:
         instances = load_data.getHardInstances(False, False)
     else:
@@ -20,8 +21,16 @@ while not done:
 ratioScore = 0
 waste = 0
 totalOpt = 0
+totalTime = 0
+
 for instance in instances:
+    # Decision to use time.perf_counter() was due to https://builtin.com/articles/timing-functions-python
+    # time.time() is apparently not as precise and timeit is generally for small bits of code
+    startTime = time.perf_counter()
     packing = randomised_best_fit.randomisedBestFit(instance["bin_capacity"], instance["weights"].copy())
+    endTime = time.perf_counter()
+
+    totalTime += (endTime - startTime)
 
     opt = instance["optimal_solution"]
     alg = len(packing)
@@ -30,10 +39,11 @@ for instance in instances:
 
     ratioScore += (alg/opt)
 
-print(totalOpt)
-print(waste)
-print((waste + totalOpt)/totalOpt)
-print(len(instances))
-print(ratioScore)
-print(ratioScore/len(instances))
+print("Time: " + str(totalTime))
+print("Total Bins used in optimal case: " + str(totalOpt))
+print("Waste: " + str(waste))
+print("Ratio of bins used by algorithm to bins used in optimal: " +  str((waste + totalOpt)/totalOpt))
+print("Number of instances used: " + str(len(instances)))
+print("Average ratio of bins used by algorithm to bins used in optimal case: " + str(ratioScore/len(instances)))
+
 
