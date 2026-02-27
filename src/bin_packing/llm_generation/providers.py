40c3baaa1c53c6a42aed861ab7db3e62bb5cea2c
Joshua Raybould <joshua.raybould@outlook.com>
2026-02-27 16:00:31 +0000

    add LLM prompts

--- src/bin_packing/llm_generation/providers.py
+++ src/bin_packing/llm_generation/providers.py
@@ -1,8 +1,6 @@
 from openai import OpenAI
 from pydantic import BaseModel
-
-import bin_packing.main
-import bin_packing.utilities.load_data
+import prompts
 
 class PythonCode(BaseModel):
     reasoning: str
@@ -14,23 +12,11 @@ class PerformancePlan(BaseModel):
 
 openAIModel = "gpt-5-nano"
 
-HARD_REQUIREMENTS = "Requirements:\n " \
-    "- The main method must be called solve and take as input the bin capacity as an integer, the weights of the items in an array and a time limit in seconds in that order only. No other arguments are allowed.\n" \
-    "- It must return the packing you arrive at as a dictionary: where the key 'packing' corresponds to a list of lists, where each list contains the indexes of the items contained in it." \
-    "For example index 0 will contain all indexes of the items packed in the 0th bin." \
-    "Secondly the key 'bin_weights'  must correspond to a list of the total weight of each bin, such that index 0 corresponds to the weight of the 0th bin.\n" \
-    "- The algorithm should terminate after a set number of iterations you decide. If a time limit is provided then it must be checked at regular intervals, and if it is exceeded then the best solution so far should be returned." \
-    "- The evaluation harness seeds Python’s global RNG with random.seed(...). Therefore your code must use only the global random module for randomness. Do not call random.seed() inside solve.\n" \
-    "- Your code must not have anything in it that will run when imported.\n" \
-
 def initialOpenAIPrompt(algoName):
     client = OpenAI()
 
-    systemPrompt = "You are an experienced python developer. Reason about and write the required python code"
-
-    userPrompt = " Your task is: implement " + algoName + " for the 1D offline bin packing problem in python. " \
-        "The specifics are entirely up to you but it should arrive at very strong solutions.\n" \
-        "" + HARD_REQUIREMENTS +"\n" 
+    systemPrompt = prompts.CODE_SYSTEM_PROMPT
+    userPrompt = prompts.getInitialPrompt(algoName)
 
     # Initial attempt to get a correct algorithm
     response = client.responses.parse(
@@ -54,14 +40,8 @@ def initialOpenAIPrompt(algoName):
 def correctnessOpenAIPrompt(algoName, prevCode, error):
     client = OpenAI()
 
-    systemPrompt = "You are an experienced python developer. Reason about and fix the python code"
-    
-    userPrompt =  "The following code is for the algorithm: " + algoName + " for the 1D offline bin packing problem in python.\n" \
-        "Code: \n" \
-        "" + prevCode + "\n" \
-        "Running this gave the error: " + error + "\n" \
-        "Please fix the code. The specifics of the implementation are entirely up to you but it should arrive at very strong solutions.\n" \
-        "" + HARD_REQUIREMENTS
+    systemPrompt = prompts.CORRECTNESS_SYSTEM_PROMPT
+    userPrompt =  prompts.getCorrectnessPrompt(algoName, prevCode, error)
 
     # Prompt with the code and error in attempt to fix it
     response = client.responses.parse(
@@ -80,19 +60,13 @@ def correctnessOpenAIPrompt(algoName, prevCode, error):
 
     return event.code
 
-def performancePlanOpenAIPrompt(algoName, prevCode):
+def performancePlanOpenAIPrompt(algoName, prevCode, performanceMetrics):
     client = OpenAI()
 
-    systemPrompt = "You are an experienced python developer, with deep understanding of algorithms. Write a detailed plan of how to improve the performance of the given code."
+    systemPrompt = prompts.PLAN_SYSTEM_PROMPT
+    userPrompt = prompts.getPlanPrompt(algoName, prevCode, performanceMetrics)
 
-    userPrompt = " Your task is: plan how to improve this implementation of " + algoName + " for the 1D offline bin packing problem in python. " \
-        "implementation:\n" \
-        "" + prevCode +"\n" \
-        "The specifics are entirely up to you but when followed your plan should improve the code allowing it to reach stronger solutions in a given time.\n" \
-        "The following requirements will be given along with your plan, so your plan must not conflict with any requirement.\n" \
-        "" + HARD_REQUIREMENTS
-
-    # Initial attempt to get a correct algorithm
+    # Prompt to get plan for improving code performance
     response = client.responses.parse(
         model=openAIModel,
         input=[
@@ -103,7 +77,7 @@ def performancePlanOpenAIPrompt(algoName, prevCode):
                 ,
             },
         ],
-        text_format=PythonCode,
+        text_format=PerformancePlan,
     )
 
     event = response.output_parsed
@@ -113,16 +87,10 @@ def performancePlanOpenAIPrompt(algoName, prevCode):
 def applyPerformancePlanOpenAIPrompt(algoName, prevCode, performancePlan):
     client = OpenAI()
 
-    systemPrompt = "You are an experienced python developer. Apply the plan to improve the given code."
+    systemPrompt = prompts.APPLY_PLAN_SYSTEM_PROMPT
+    userPrompt = prompts.applyPlanPrompt(algoName, prevCode, performancePlan)
 
-    userPrompt = " Your task is: plan how to improve this implementation of " + algoName + " for the 1D offline bin packing problem in python. " \
-        "implementation:\n" \
-        "" + prevCode +"\n" \
-        "" + performancePlan + "\n" \
-        "The specifics are entirely up to you but when followed your plan should improve the code allowing it to reach stronger solutions in a given time.\n" \
-        "" + HARD_REQUIREMENTS
-
-    # Initial attempt to get a correct algorithm
+    # Prompt to apply performance plan
     response = client.responses.parse(
         model=openAIModel,
         input=[
@@ -140,23 +108,14 @@ def applyPerformancePlanOpenAIPrompt(algoName, prevCode, performancePlan):
 
     return event.code
 
-def performanceOpenAIPrompt(algoName, prevCode, timeTaken, avgRatio, extraBins):
+def performanceOpenAIPrompt(algoName, prevCode, performanceMetrics):
     client = OpenAI()
 
-    systemPrompt = "You are an experienced python developer. Reason about and improve the python code"
-    
-    userPrompt =  "The following code is for the algorithm: " + algoName + " for the 1D offline bin packing problem in python.\n" \
-        "Code: \n" \
-        "" + prevCode + "\n" \
-        "Here are some peroformance details: \n" \
-        "Average ratio of algorithm solution to that of the optimal (higher is worse): " + str(avgRatio) + "\n" \
-        "Extra bins used compared to optimal (or lower bound): " + str(extraBins) + "\n" \
-        "Time taken: " + str(timeTaken) + "out of the provided 100s (note: it is preferable to use all of the time to get better solutions, using extra time is not punished the algorithm will simply be stopped at 100s)" \
-        "Please improve the code and change parameters where you see fit. The specifics of the implementation are entirely up to you but it should arrive at very strong solutions.\n" \
-        "" + HARD_REQUIREMENTS
+    systemPrompt = prompts.IMPROVEMENT_SYSTEM_PROMPT
+    userPrompt =  prompts.improvePerformancePrompt(algoName, prevCode, performanceMetrics)
 
 
-    # Prompt with the code and error in attempt to fix it
+    # Prompt with the code and performance results to improve performance
     response = client.responses.parse(
         model=openAIModel,
         input=[
